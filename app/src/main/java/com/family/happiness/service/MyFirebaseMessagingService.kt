4d2f7384d63997ce1d9821acfa42d10211730624
YiSuHo <xon235@naver.com>
2021-06-05 01:12:44 +0900

    Implement FCM onMessageReceived

--- app/src/main/java/com/family/happiness/service/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/family/happiness/service/MyFirebaseMessagingService.kt
@@ -1,16 +1,66 @@
 package com.family.happiness.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.navigation.NavDeepLinkBuilder
 import com.family.happiness.HappinessApplication
+import com.family.happiness.PreferenceKeys
+import com.family.happiness.R
+import com.family.happiness.dataStore
 import com.family.happiness.network.request.RegisterFcmData
+import com.family.happiness.ui.MainActivity
+import com.family.happiness.ui.MainActivityViewModel
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 
-class MyFirebaseMessagingService: FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Timber.d("Refreshed token: $token")
         runBlocking { (application as HappinessApplication).userRepository.insertFcmToken(token) }
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Timber.d("From: ${remoteMessage.from}")
+        runBlocking { (application as HappinessApplication).mailRepository.syncMail() }
+        sendNotification()
+    }
+
+    private fun sendNotification() {
+        val pendingIntent = NavDeepLinkBuilder(this)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.mailFragment)
+            .createPendingIntent()
+
+        val channelId = "channelId"
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_mail)
+            .setContentTitle("New Mail!")
+            .setContentText("You have a new mail")
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId,
+                "New mail notification channel",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        notificationManager.notify(0, notificationBuilder.build())
     }
 }
