dc0f4bc72221da382029ec5b44977770c8546756
YiSuHo <xon235@naver.com>
2021-07-25 22:49:08 +0900

    Initial Creation of BaseDao.kt

--- app/src/main/java/com/family/happiness/room/user/UserDao.kt
+++ app/src/main/java/com/family/happiness/room/user/UserDao.kt
@@ -1,46 +1,17 @@
 package com.family.happiness.room.user
 
-import androidx.room.*
+import androidx.room.Dao
+import androidx.room.Query
+import com.family.happiness.room.BaseDao
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
-interface UserDao {
+abstract class UserDao: BaseDao<User>("user") {
 
     @Query("SELECT * FROM user")
-    fun getAll(): Flow<List<User>>
+    abstract fun selectAll(): Flow<List<User>>
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(users: List<User>): List<Long>
+    fun getAll() = selectAll()
 
-    // TODO make base Dao
-
-    @Query("DELETE FROM user WHERE id NOT IN (:userIds)")
-    suspend fun deleteNotIn(userIds: List<String>)
-
-    @Update
-    suspend fun update(users: List<User>)
-
-    @Transaction
-    suspend fun upsert(users: List<User>) {
-        val insertResult = insert(users)
-        val updateList: MutableList<User> = ArrayList()
-
-        for (i in insertResult.indices) {
-            if (insertResult[i] == -1L) {
-                updateList.add(users[i])
-            }
-        }
-
-        if (updateList.isNotEmpty()) {
-            update(updateList)
-        }
-
-    }
-
-    @Transaction
-    suspend fun sync(users: List<User>) {
-        deleteNotIn(users.map { it.id })
-        upsert(users)
-    }
+    suspend fun deleteNotIn(users: List<User>) = deleteAll()
 }
