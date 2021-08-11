b61e437830d1b2ad8bc576ac52b4fd1f7a53ddfa
YiSuHo <xon235@naver.com>
2021-08-11 18:38:58 +0900

    Change DiffCallback Logic

--- app/src/main/java/com/family/happiness/adapter/FamilyListAdapter.kt
+++ app/src/main/java/com/family/happiness/adapter/FamilyListAdapter.kt
@@ -26,11 +26,11 @@ class FamilyListAdapter()
 
     companion object DiffCallback : DiffUtil.ItemCallback<User>() {
         override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem.id === newItem.id
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem.id == newItem.id
+            return oldItem == newItem
         }
     }
 
--- app/src/main/java/com/family/happiness/adapter/TagListAdapter.kt
+++ app/src/main/java/com/family/happiness/adapter/TagListAdapter.kt
@@ -25,11 +25,11 @@ class TagListAdapter(
 
     companion object DiffCallback : DiffUtil.ItemCallback<User>() {
         override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem.id === newItem.id
+            return oldItem.id == newItem.id
         }
 
         override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
-            return oldItem.id == newItem.id
+            return oldItem == newItem
         }
     }
 
